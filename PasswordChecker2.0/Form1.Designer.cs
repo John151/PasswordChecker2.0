141e661c2cafd64a3345941c346fce617601fbc2
John Cokins <il7148pg@go.minnstate.edu>
2020-02-12 21:47:10 -0600

    Add project files.

---
+++ PasswordChecker2.0/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PasswordChecker2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblEstimateStrength = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Example Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(174, 44);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(190, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // lblEstimateStrength
+            // 
+            this.lblEstimateStrength.Location = new System.Drawing.Point(117, 106);
+            this.lblEstimateStrength.Name = "lblEstimateStrength";
+            this.lblEstimateStrength.Size = new System.Drawing.Size(176, 23);
+            this.lblEstimateStrength.TabIndex = 2;
+            this.lblEstimateStrength.Text = "Estimate Strength";
+            this.lblEstimateStrength.UseVisualStyleBackColor = true;
+            this.lblEstimateStrength.Click += new System.EventHandler(this.lblEstimateStrength_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(142, 168);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Padding = new System.Windows.Forms.Padding(10);
+            this.lblResult.Size = new System.Drawing.Size(20, 33);
+            this.lblResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 220);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblEstimateStrength);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button lblEstimateStrength;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ PasswordChecker2.0/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordChecker2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblEstimateStrength_Click(object sender, EventArgs e)
+        {
+            string password = txtPassword.Text;
+            int complexity = 0;
+
+            if (password.Any(char.IsDigit))
+            {
+                complexity++;
+            }
+            if (password.Any(char.IsLetter))
+            {
+                complexity++;
+            }
+            if (password.Any(char.IsLower))
+            {
+                complexity++;
+            }
+            if (password.Any(char.IsUpper))
+            {
+                complexity++;
+            }
+            if (password.Any(char.IsPunctuation))
+            {
+                complexity++;
+            }
+
+            if (password.Length > 10 && complexity > 4)
+            {
+                lblResult.Text = "Good";
+                lblResult.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                lblResult.Text = "Bad";
+                lblResult.BackColor = Color.Red;
+            }
+        }
+    }
+}
